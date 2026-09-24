fe1dc4b28232bd676cf8ea2073e9c8fa581c27de
Jacob Beers <jbeers@ortussolutions.com>
2026-09-23 23:26:46 -0500

    feat: install MatchBox releases

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -7,16 +7,14 @@ if [[ -z "$MATCHBOX_BIN" && -x "$ROOT/../matchbox/target/release/matchbox" ]]; t
     MATCHBOX_BIN="$ROOT/../matchbox/target/release/matchbox"
 fi
 MATCHBOX_BIN="${MATCHBOX_BIN:-$(command -v matchbox)}"
-TEMP_HOME="$(mktemp -d)"
 REPORT_DIR="$ROOT/build/test-results"
-trap 'rm -rf "$TEMP_HOME"' EXIT
 
 cd "$ROOT"
 mkdir -p build
 "$MATCHBOX_BIN" --target native --output build/mvm src/mvm.bxs
 chmod +x build/mvm
 rm -rf "$REPORT_DIR"
-MVM_HOME="$TEMP_HOME" MVM_BIN="$ROOT/build/mvm" ./testbox/run \
+MVM_BIN="$ROOT/build/mvm" ./testbox/run \
     --directory=tests.specs \
     --reportpath="$REPORT_DIR" \
     --verbose
